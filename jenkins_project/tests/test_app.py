7edc6a0d17b89d5512026a4950ec23f68722720a
Taline <tws04@mail.aub.edu>
2024-11-07 14:54:46 +0000

    Update Jenkins for correct path

---
+++ jenkins_project/tests/test_app.py
@@ -0,0 +1,10 @@
+
+import unittest
+from app import greet
+
+class TestApp(unittest.TestCase):
+    def test_greet(self):
+        self.assertEqual(greet("World"), "Hello, World from Taline Slim!")
+
+if __name__ == "__main__":
+    unittest.main()
